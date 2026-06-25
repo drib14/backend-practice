6681fdfac3537894646f5d59453a82d028070d08
drib14 <jhondribramirez7@gmail.com>
2026-06-26 00:36:17 +0800

    changes

--- server/models/Lesson.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('lesson', new mongoose.Schema({
-    title: { type: String, required: true },
-    module: { type: String, required: true },
-    order: { type: Number, required: true },
-    content: { type: String, required: true },
-    codeSnippet: String,
-    language: String
-}));
--- server/models/Progress.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('progress', new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
-    completedLessons: [{ lessonId: { type: mongoose.Schema.Types.ObjectId, ref: 'lesson' }, completedAt: { type: Date, default: Date.now } }],
-    completedQuizzes: [{ quizId: { type: mongoose.Schema.Types.ObjectId, ref: 'quiz' }, score: Number, completedAt: { type: Date, default: Date.now } }]
-}));
